5e2f055a56c68b356ec51186f8af3302a62d8b9a
Paweł Urbańczyk <pawel.urbanczyk90@gmail.com>
2019-05-30 16:34:08 +0200

    Testing a Form Submit

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -45,4 +45,32 @@ class DefaultControllerTest extends WebTestCase
 
     }
 
+    public function testItGrowsADinosaurFromSpecification()
+    {
+        $this->loadFixtures([
+            LoadBasicParkData::class,
+            LoadSecurityData::class
+        ]);
+
+        $client = $this->makeClient();
+        $client->followRedirects();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertStatusCode(200, $client);
+
+        $form = $crawler->selectButton('Grow dinosaur')->form();
+        $form['enclosure']->select(3);
+        $form['specification']->setValue('large herbivore');
+
+        $client->submit($form);
+
+        $this->assertContains(
+            'Grew a large herbivore in enclosure #3',
+            $client->getResponse()->getContent()
+        );
+
+
+    }
+
 }
